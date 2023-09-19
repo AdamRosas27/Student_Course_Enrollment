af97e027cffa9fda0d643f6354a0982cbbb988a7
Adam Rosas <adamrosas27@gmail.com>
2023-09-19 15:53:40 -0700

    Add methods to Student class and Course class

--- src/main.py
+++ src/main.py
@@ -6,13 +6,31 @@ class Courses:
         self.enrolled_students = []
         self.schedule = schedule
 
+    def add_student(self, student_id):
+        pass
+
+    def drop_student(self, student_id):
+        pass
+
+    def view_roster(self):
+        pass
+
 
 class Student:
 
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+    def __init__(self, student_id, student_name):
+        self.student_id = student_id
+        self.student_name = student_name
         self.enrolled_courses = set()
+
+    def add_course(self, course_id):
+        pass
+
+    def drop_course(self, course_id):
+        pass
+
+    def view_schedule(self):
+        pass
 
 
 class Registration:
@@ -22,9 +40,20 @@ class Registration:
         self.students = {}
 
     # Create a method that adds a course to a students class list
+    def add_course(self, student_id, course_id):
+        if student_id not in self.students and course_id in Courses:
+            self.students[student_id] = course_id
+            self.students.enroll
 
     # Create a method that removes a course from a students class list
 
+    def drop_course(self, student_id, course_id):
+        pass
+
     # Create a method that allows the student to view their current course schedules
+    def view_schedule(self):
+        pass
 
     # Create a method that returns the roster of the course which returns a list of students enrolled in the course
+    def course_roster(self):
+        pass
