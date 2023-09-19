09e9e6f0c6714295231f645f2f8474492513e193
Adam Rosas <adamrosas27@gmail.com>
2023-09-19 16:23:27 -0700

    Add student, course, registrar, avl tree, and avl node class

--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
+# Import collections to be able to use the set data structure
+from collections import set as Set
+
 # Add a class that will represent the student
+
+
+class Student:
+    pass
 
 # Add a class that will represent the course
 
+
+class Course:
+    pass
+
 # Add a class that will represent the registrar system
 
+
+class Registrar:
+    pass
+
 # Add a class that will represent the AVL tree and its methods
+
+
+class AVLTree:
+    pass
+
+# Add a class that will represent the nodes of the AVL tree
+
+
+class AVLNode:
+    pass
