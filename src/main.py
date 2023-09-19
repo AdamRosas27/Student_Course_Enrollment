92ea17e93ba8adc8b0ce3e0857cba52862768120
Adam Rosas <adamrosas27@gmail.com>
2023-09-18 17:50:34 -0700

    Add class 'Courses' and class 'Student'.

--- src/main.py
+++ src/main.py
@@ -5,18 +5,21 @@
 
 class Courses:
 
-    def __init__(self, course, student):
-        self.course = course
-        self.student = student
+    def __init__(self, course_name, course_id, schedule):
+        self.course_name = course_name
+        self.course_id = course_id
+        self.enrolled_students = []
+        self.schedule = schedule
 
     # Create a method that returns the roster of the course which returns a list of students enrolled in the course
 
 
 class Student:
 
-    def __init__(self, ID, courses=[]):
-        self.ID = ID
-        self.courses = courses
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.enrolled_courses = set()
 
     # Create a method that adds a course to a students class list
 
